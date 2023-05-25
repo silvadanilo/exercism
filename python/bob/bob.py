0664fc44dfaf67d68624623a9f866aaaa537bc06
danilo silva <badkill82@gmail.com>
2023-05-25 18:30:23 +0200

    refactor: [python] use private method convention

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,19 +1,27 @@
+import re
+
 def hey(phrase):
+    return "Whatever";
+
+def response(phrase):
     phrase = phrase.strip()
 
-    if not phrase:
+    if("" == phrase):
         return "Fine. Be that way!"
 
-    if (is_yell(phrase)):
-        return "Whoa, chill out!"
+    if(_is_yell(phrase)):
+        if(phrase[-1] == '?'):
+            return "Calm down, I know what I'm doing!";
+        else:
+            return "Whoa, chill out!";
 
-    if (is_question(phrase)):
-        return "Sure."
+    if(_is_question(phrase)):
+        return "Sure.";
 
-    return "Whatever."
+    return "Whatever.";
 
-def is_question(phrase):
-    return phrase.endswith('?')
+def _is_yell(phrase):
+    return phrase.isupper();
 
-def is_yell(phrase):
-    return phrase.isupper()
+def _is_question(phrase):
+    return phrase.endswith('?');
